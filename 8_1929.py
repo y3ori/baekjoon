09e29f60096adc477b59b8410b69e6cd798d37d9
y3ori <wintennf99@gmail.com>
2022-07-23 22:51:03 +0900

    second commit

--- 8_1929.py
+++ 8_1929.py
@@ -1,11 +1,14 @@
+def is_prime_number(x) :
+    if x == 1 :
+        return False
+    else :
+        for i in range(2, int(x**(1/2)+1)) :
+            if x % i == 0 :
+                return False
+        return True
+
 m, n = map(int,input().split())
 
-
 for i in range(m, n+1) :
-    if i == 1 :
-        continue
-    for j in range(2, int(i**0.5)+1) :
-        if i % j == 0 :
-            break
-    else :
+    if is_prime_number(i) :
         print(i)
