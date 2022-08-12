83d02d0659d04b255e83c7e06a613543d8448f42
y3ori <wintennf99@gmail.com>
2022-08-12 23:28:34 +0900

    second commit

--- 나도코딩 8퀴즈.py
+++ 나도코딩 8퀴즈.py
@@ -1,6 +1,36 @@
-for i in range(1, 11):
-    with open(str(i) + "주차.txt", "w", encoding="utf8") as report_file:
-        report_file.write("- {0} 주차 주간보고 -".format(i))
-        report_file.write("\n부서:")
-        report_file.write("\n이름:")
-        report_file.write("\n업무요약:")
+# Quiz 주어진 코드를 활용하여 부동산 프로그램을 작성하시오.
+
+# 3대의 매물이 있다.
+
+# 강남 아파트 매매 10억 / 2010년
+# 마포 오피스텔 전쎄 5억 / 2007년
+# 송파 빌라 월세 500/50 / 2000년
+
+
+class House:
+    # 매물 초기화
+    def __init__(self, location, house_type, deal_type, price, comp_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.comp_year = comp_year
+
+    # 매물 정보 표시
+    def show_detail(self):
+        print(self.location, self.house_type,
+              self.deal_type, self.price, self.comp_year)
+
+
+houses = []
+house1 = House("강남", "아파트", "매매", "10억", "2010년")
+house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+house3 = House("송파", "빌라", "월세", "500/50", "2000년")
+
+houses.append(house1)
+houses.append(house2)
+houses.append(house3)
+
+print("총 {0} 대의 매물이 있습니다".format(len(houses)))
+for house in houses:
+    house.show_detail()
--- 나도코딩 9퀴즈.py
+++ 나도코딩 9퀴즈.py
@@ -1,36 +1,31 @@
-# Quiz 주어진 코드를 활용하여 부동산 프로그램을 작성하시오.
-
-# 3대의 매물이 있다.
-
-# 강남 아파트 매매 10억 / 2010년
-# 마포 오피스텔 전쎄 5억 / 2007년
-# 송파 빌라 월세 500/50 / 2000년
+class soldouterror(Exception):
+    pass
 
 
-class House:
-    # 매물 초기화
-    def __init__(self, location, house_type, deal_type, price, comp_year):
-        self.location = location
-        self.house_type = house_type
-        self.deal_type = deal_type
-        self.price = price
-        self.comp_year = comp_year
+chicken = 10
+waiting = 1
 
-    # 매물 정보 표시
-    def show_detail(self):
-        print(self.location, self.house_type,
-              self.deal_type, self.price, self.comp_year)
+while True:
+    try:
+        print("[남은치킨 : {0}]".format(chicken))
+        order = int(input("치킨 몇마리 주문 하시겠습니까?"))
+        if order > chicken:
+            print("재료가 부족합니다.")
 
+        elif order <= 0:
+            raise ValueError
 
-houses = []
-house1 = House("강남", "아파트", "매매", "10억", "2010년")
-house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
-house3 = House("송파", "빌라", "월세", "500/50", "2000년")
+        else:
+            print("[대기번호 {0}] {1} 마리 주문 완료되었습니다.".format(waiting, order))
+            waiting += 1
+            chicken -= order
 
-houses.append(house1)
-houses.append(house2)
-houses.append(house3)
+        if chicken == 0:
+            raise soldouterror
 
-print("총 {0} 대의 매물이 있습니다".format(len(houses)))
-for house in houses:
-    house.show_detail()
+    except ValueError:
+        print("잘못된 값을 입력하였습니다.")
+
+    except soldouterror:
+        print("재고소진")
+        break
